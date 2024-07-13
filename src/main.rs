c15d4419747dd75fde8ea19adc903a6518dc6049
Chris Ohk <utilforever@gmail.com>
2024-07-13 21:45:45 +0900

    tests: Add unit test code for function 'hello()'

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,27 @@
 extern crate rocket;
 
 #[get("/")]
-fn index() -> &'static str {
+fn hello() -> &'static str {
     "Hello, world!"
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build().mount("/", routes![hello])
+}
+
+#[cfg(test)]
+mod test {
+    use super::rocket;
+    use rocket::local::blocking::Client;
+    use rocket::http::Status;
+
+    #[test]
+    fn test_hello() {
+        let client = Client::tracked(rocket()).expect("valid rocket instance");
+        let response = client.get(uri!(super::hello)).dispatch();
+
+        assert_eq!(response.status(), Status::Ok);
+        assert_eq!(response.into_string().unwrap(), "Hello, world!");
+    }
 }
