2e5a198d228fb50855c74001ce36f7e8a3904bd1
Chris Ohk <utilforever@gmail.com>
2024-07-24 23:46:14 +0900

    test: Implement integration test for a health-check endpoint

--- src/main.rs
+++ src/main.rs
@@ -10,3 +10,20 @@ fn health_check() {
 fn rocket() -> _ {
     rocket::build().mount("/", routes![health_check])
 }
+
+#[cfg(test)]
+mod test {
+    use super::rocket;
+    use rocket::http::Status;
+    use rocket::local::blocking::Client;
+    use std::io::Read;
+
+    #[test]
+    fn test_hello() {
+        let client = Client::tracked(rocket()).expect("Failed to create client");
+        let response = client.get(uri!(super::health_check)).dispatch();
+
+        assert_eq!(response.status(), Status::Ok);
+        assert_eq!(response.bytes().count(), 0);
+    }
+}
