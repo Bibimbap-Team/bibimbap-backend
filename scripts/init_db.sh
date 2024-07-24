caeaa7818bf09258176f6a263dc7aefff00ff4c8
Chris Ohk <utilforever@gmail.com>
2024-07-24 15:40:35 +0900

    feat: Update script to keep pinging Postgres until it's ready

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -22,3 +22,15 @@ docker run \
   -d postgres \
   postgres -N 1000
   # ^ Increased maximum number of connections for testing purposes
+
+export PGPASSWORD="${DB_PASSWORD}"
+until psql -h "${DB_HOST}" -U "${DB_USER}" -p "${DB_PORT}" -d "postgres" -c '\q'; do
+  >&2 echo "Postgres is still unavailable - sleeping"
+  sleep 1
+done
+
+>&2 echo "Postgres is up and running on port ${DB_PORT}!"
+
+DATABASE_URL=postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}
+export DATABASE_URL
+sqlx database create
