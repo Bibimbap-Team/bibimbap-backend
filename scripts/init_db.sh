870e4460b71bd472977a8bc05eccbada86bde1ff
Chris Ohk <utilforever@gmail.com>
2024-07-24 16:03:52 +0900

    feat: Update script for running migrations

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -16,15 +16,10 @@ if ! [ -x "$(command -v sqlx)" ]; then
   exit 1
 fi
 
-# Check if a custom user has been set, otherwise default to 'postgres'
 DB_USER=${POSTGRES_USER:=postgres}
-# Check if a custom password has been set, otherwise default to 'password'
 DB_PASSWORD="${POSTGRES_PASSWORD:=password}"
-# Check if a custom database name has been set, otherwise default to 'bibimbap'
 DB_NAME="${POSTGRES_DB:=bibimbap}"
-# Check if a custom port has been set, otherwise default to '5432'
 DB_PORT="${POSTGRES_PORT:=5432}"
-# Check if a custom host has been set, otherwise default to 'localhost'
 DB_HOST="${POSTGRES_HOST:=localhost}"
 
 # Launch Postgres using Docker
@@ -43,8 +38,11 @@ until psql -h "${DB_HOST}" -U "${DB_USER}" -p "${DB_PORT}" -d "postgres" -c '\q'
   sleep 1
 done
 
->&2 echo "Postgres is up and running on port ${DB_PORT}!"
+>&2 echo "Postgres is up and running on port ${DB_PORT} - running migrations now!"
 
 DATABASE_URL=postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}
 export DATABASE_URL
 sqlx database create
+sqlx migrate run
+
+>&2 echo "Postgres has been migrated, ready to go!"
