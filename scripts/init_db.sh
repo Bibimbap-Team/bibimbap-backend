a0a56f2b429916db5ea0e174122edf4ebca41e4f
Chris Ohk <utilforever@gmail.com>
2024-08-02 00:00:33 +0900

    chore: Fix grammar by changing 'allow to' to 'allow ~ing'

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -23,7 +23,7 @@ DB_PORT="${POSTGRES_PORT:=5432}"
 DB_HOST="${POSTGRES_HOST:=localhost}"
 
 # Launch Postgres using Docker
-# Allow to skip Docker if a dockerized Postgres database is already running
+# Allow skipping Docker if a dockerized Postgres database is already running
 if [[ -z "${SKIP_DOCKER}" ]]
 then
   docker run \
