a741b0a27f149125a0b1c1bdde9e0cb43d6d7d60
Chris Ohk <utilforever@gmail.com>
2024-08-03 16:44:38 +0900

    refactor: Change code to load library

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,9 @@
-#[macro_use]
-extern crate rocket;
+use bibimbap_backend::run;
 
-#[get("/health_check")]
-fn health_check() {
-    ()
-}
-
-#[launch]
-fn rocket() -> _ {
-    rocket::build().mount("/", routes![health_check])
-}
-
-#[cfg(test)]
-mod test {
-    use super::rocket;
-    use rocket::http::Status;
-    use rocket::local::blocking::Client;
-    use std::io::Read;
-
-    #[test]
-    fn health_check_works() {
-        let client = Client::tracked(rocket()).expect("Failed to create client");
-        let response = client.get(uri!(super::health_check)).dispatch();
-
-        assert_eq!(response.status(), Status::Ok);
-        assert_eq!(response.bytes().count(), 0);
+#[rocket::main]
+async fn main() {
+    if let Err(e) = run().launch().await {
+        println!("Failed to launch Rocket: {e}");
+        drop(e);
     }
 }
